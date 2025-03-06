5f1bc8f772c3447ab597ee56a92380226d98f87b
sarah Liu <sarahliurz@gmail.com>
2025-03-06 19:23:03 +0000

    Initial commit.

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import express from"express";
 const app = express();
 const port = 3000;
 
+app.get ("/", (req,res)=>{
+    res.render("index.ejs");
+})
 
-
-
+app.use(express.static("public"))
 
 
 
