eefaf4c1c27498fc538d87187c219725bbda1ecf
sarah Liu <sarahliurz@gmail.com>
2025-03-13 23:02:53 +0000

    finish the creating function

--- index.js
+++ index.js
@@ -1,17 +1,54 @@
 import express from"express";
+import bodyParser from "body-parser";
+import mongoose from 'mongoose';
+
+
+
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"))
+app.use( bodyParser.json() );  
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let blogPosts=[];
+
+function displayBlogs() {
+  
+$("main").append(
+
+
+
+);
+  
+}
+
+function submitContent() {
+  const content = document.getElementById('editor').innerHTML;
+  blogPosts.push(content); 
+  displayBlogs();
+}
+
+app.get("/submit",(req,res,next)=>{
+ res.redirect("/");
+ submitContent();
+})
 app.get ("/", (req,res)=>{
-    res.render("index.ejs");
+    res.render("index.ejs", { title: null, content:null });
 })
 
-app.use(express.static("public"))
+app.get ("/contact", (req,res)=>{
+    res.render("contact.ejs");
+})
 
 
+app.get("/create", (req, res) => {
+    res.render("create.ejs");
+  });
 
+app.post("/submit",(req,res)=>{
 
-
+})
 
 
 app.listen(port, () => {
