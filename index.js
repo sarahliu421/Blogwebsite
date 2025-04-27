ebc5ecd3b2ad7f0c94248ebc4419843f8fc3f1ae
sarah Liu <sarahliurz@gmail.com>
2025-04-27 19:17:17 +0100

    view post

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 let blogs=[];
 
 app.get ("/", (req,res)=>{
-    res.render("index.ejs",{blogs: blogs.title});
+    res.render("index.ejs",{blogs: blogs, });
 })
 
 app.get ("/contact", (req,res)=>{
@@ -30,16 +30,28 @@ app.get("/create", (req, res) => {
 
 app.post("/submit",(req,res)=>{
   const title=req.body["title"];
-  const id= title;
+  const id= Date.now().toString();;
   const content= req.body["content"]
   blogs.push({id,title, content});
   res.redirect("/");
 })
 
 app.get("/blog/:id",(req,res)=>{
-  res.render("post.ejs");
+  const blogId = req.params.id;
+  const certainBlog=blogs.find((element) => element.id===blogId);
+
+  console.log("Blog ID:", req.params.id);
+  console.log("Found blog:", certainBlog);
+  if(certainBlog){
+    res.render("blog.ejs",{blog:certainBlog});
+  }
+  else{
+    res.status(404).send("Blog not found")
+  }
 
 })
+
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
