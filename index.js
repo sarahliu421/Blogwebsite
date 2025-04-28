f5547879dfec7b4268464f7747f467befafcf173
sarah Liu <sarahliurz@gmail.com>
2025-04-28 23:29:06 +0100

    add delete function

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from"express";
 import bodyParser from "body-parser";
 
 
-
+let blogs=[];
 
 const app = express();
 const port = 3000;
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
-let blogs=[];
+
 
 app.get ("/", (req,res)=>{
     res.render("index.ejs",{blogs: blogs, });
@@ -51,7 +51,17 @@ app.get("/blog/:id",(req,res)=>{
 
 })
 
+app.post("/delete/",(req,res)=>{
+const blogId= req.body["id"];
+console.log("before deletion",blogs)
+blogs= blogs.filter((element)=>element.id !== blogId);//const blogsJavaScript 会认为：左边的 const blogs 是你在函数作用域中新声明一个局部变量；但是右边你又想用 blogs，而这个变量还没“初始化”；所以解释器会报错：
+console.log("after deletion",blogs)
+res.redirect("/");
+//if(afterDelete){
+  //res.render("index.ejs",{blogs:afterDelete});
+//} 不懂为什么就不行
 
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
